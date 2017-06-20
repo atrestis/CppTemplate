79e75cf8bd75f4ba51d41ed917e68afd3c5a5ab0
Atrestis Karalis <atrestis@gmail.com>
2017-06-20 13:04:45 +0300

    added a newline in bld script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#Use this script to build the cpp project you need to using make
 
 echo "Stating the start of building..."
 make ./hello.cpp 
 make run 
+echo #printing a new line
 echo "Stating the end of the building..."
